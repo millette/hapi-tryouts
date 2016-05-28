b82034c5db91f04f411d0f9c2e88c57ddbc52a10
Robin Millette <robin@millette.info>
2016-05-27 21:09:49 -0400

    Hello world server with promises.

--- server.js
+++ server.js
@@ -2,13 +2,20 @@
 
 const Glue = require('glue')
 
-const manifest = require('./manifest.json')
-
-Glue.compose(manifest, { relativeTo: __dirname })
+Glue.compose(require('./manifest.json'))
   .then((server) => {
-    server.start(() => {
-      console.log('hapi days!')
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler: (request, reply) => {
+        reply('Hello, world!')
+      }
     })
+    server.start()
+    return server
+  })
+  .then((server) => {
+    console.log('hapi days!', server.info.uri)
   })
   .catch((err) => {
     console.error('ERROR:', err)
