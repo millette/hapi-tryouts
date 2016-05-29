f65073154449753a7288b1fcd19a4507d0627795
Robin Millette <robin@millette.info>
2016-05-29 18:53:44 -0400

    Add api (bedwetter) route.

---
+++ routes/api-users.js
@@ -0,0 +1,17 @@
+'use strict'
+
+exports.register = require('../lib/utils').routePlugin(
+  'users',
+  [
+    {
+      method: 'GET',
+      path: '/users/{id}',
+      handler: { bedwetter: null }
+    },
+    {
+      method: 'GET',
+      path: '/users',
+      handler: { bedwetter: null }
+    }
+  ]
+)
