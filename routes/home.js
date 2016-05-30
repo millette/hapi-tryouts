addcf603b0d1d53475b492b308e01fb15d860773
Robin Millette <robin@millette.info>
2016-05-30 11:12:11 -0400

    Github login, forced on home page.

--- routes/home.js
+++ routes/home.js
@@ -4,11 +4,15 @@ exports.register = require('../lib/utils').routePlugin(
   {
     name: 'home',
     version: '0.0.0',
-    dependencies: 'vision'
+    dependencies: ['login', 'vision']
   },
   [{
     method: 'GET',
     path: '/',
-    handler: (request, reply) => reply.view('home', { request: request, reply: reply })
+    // handler: (request, reply) => reply.view('home', { request: request, reply: reply, profile: request.auth.credentials.profile })
+    config: {
+      auth: 'session',
+      handler: (request, reply) => reply.view('home', { request: request, reply: reply, profile: request.auth.credentials.profile })
+    }
   }]
 )
---
+++ routes/login.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const secure = false
+const cookiePassword = 'cookie_encryption_password_secure'
+
+exports.register = (server, options, next) => {
+  server.auth.strategy('session', 'cookie', {
+    password: cookiePassword,
+    redirectTo: '/login/github',
+    isSecure: secure     // Terrible idea but required if not using HTTPS especially if developing locally
+  })
+
+  // Declare an authentication strategy using the bell scheme
+  // with the name of the provider, cookie encryption password,
+  // and the OAuth client credentials.
+  server.auth.strategy('github', 'bell', {
+    provider: 'github',
+    password: cookiePassword,
+    clientId: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    isSecure: secure     // Terrible idea but required if not using HTTPS especially if developing locally
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    config: {
+      auth: 'session',
+      handler: (request, reply) => {
+        return reply(`Hello, ${request.auth.credentials.profile.displayName}!`)
+      }
+    }
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/github',
+    config: {
+      auth: 'github',
+      handler: (request, reply) => {
+        if (!request.auth.isAuthenticated) {
+          return reply('Authentication failed due to: ' + request.auth.error.message)
+        }
+        console.log('AUTH:', request.auth)
+        request.cookieAuth.set(request.auth.credentials)
+        return reply.redirect('/')
+      }
+    }
+  })
+
+  next()
+}
+
+exports.register.attributes = {
+  name: 'login',
+  version: '0.0.0',
+  dependencies: ['bell', 'hapi-auth-cookie']
+}
